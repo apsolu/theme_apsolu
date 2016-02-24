a34da38fd41dbd427cc79cadb910744609a728d1
Julien Boulen <julien.boulen@univ-rennes2.fr>
2016-02-24 12:02:35 +0100

    feat(core): initialise le module theme_apsolu

---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * APSOLU theme, based on Bootstrap theme.
+ *
+ * @package    theme_apsolu
+ * @copyright  2016 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$THEME->name = 'apsolu';
+
+// The only thing you need to change in this file when copying it to
+// create a new theme is the name above. You also need to change the name
+// in version.php and lang/en/theme_clean.php as well.
+
+$THEME->doctype = 'html5';
+$THEME->parents = array('bootstrap');
+$THEME->sheets = array('custom');
+$THEME->supportscssoptimisation = false;
+$THEME->yuicssmodules = array();
+$THEME->enable_dock = false;
+$THEME->editor_sheets = array();
+
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->csspostprocess = 'theme_clean_process_css';
---
+++ settings.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * APSOLU theme, based on Bootstrap theme.
+ *
+ * @package    theme_apsolu
+ * @copyright  2016 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    // Invert Navbar to dark background.
+    $name = 'theme_apsolu/invert';
+    $title = get_string('invert', 'theme_apsolu');
+    $description = get_string('invertdesc', 'theme_apsolu');
+    $setting = new admin_setting_configcheckbox($name, $title, $description, 0);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Logo file setting.
+    $name = 'theme_apsolu/logo';
+    $title = get_string('logo', 'theme_apsolu');
+    $description = get_string('logodesc', 'theme_apsolu');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_apsolu/customcss';
+    $title = get_string('customcss', 'theme_apsolu');
+    $description = get_string('customcssdesc', 'theme_apsolu');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Footnote setting.
+    $name = 'theme_apsolu/footnote';
+    $title = get_string('footnote', 'theme_apsolu');
+    $description = get_string('footnotedesc', 'theme_apsolu');
+    $default = '';
+    $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+}
