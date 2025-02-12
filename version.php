e239a2ef256d6c390c216da6a0e5e5cfbe19f155
Julien Boulen <julien.boulen@univ-rennes2.fr>
2025-02-12 10:31:56 +0100

    feat(core): publie la version 4.4.6

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2024121100;
+$plugin->version = 2025021200;
 
-$plugin->release = '4.4.5';
+$plugin->release = '4.4.6';
 
 // Requires Moodle 4.4.0.
 $plugin->requires = 2024042200;
@@ -39,7 +39,7 @@ $plugin->component = 'theme_apsolu';
 
 // This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
-    'local_apsolu' => 2024121100,
+    'local_apsolu' => 2025021200,
 ];
 
 // This is a stable release.
